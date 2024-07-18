92cb63a69040e3cc77e0458886a871456af5ecfa
Xuefeng Ding <dingxf@ihep.ac.cn>
2024-07-18 09:05:52 +0800

    add lecture 1 and 2

---
+++ lecture-2/hi.cxx
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  std::cout << "hi" << std::endl;
+
+  int a = 3;
+  std::cout << "a is " << a << std::endl;
+  a += 2;
+  std::cout << "a now is " << a << std::endl;
+  return 0;
+}
